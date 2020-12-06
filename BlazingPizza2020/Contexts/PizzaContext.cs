41c3cd79ee089d029745ec71749519763c6bdf12
playerblade <evotar156@gmail.com>
2020-12-06 16:46:22 -0400

    my first commit

---
+++ BlazingPizza2020/Contexts/PizzaContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazingPizza2020.Models;
+
+namespace BlazingPizza2020.Contexts
+{
+    public class PizzaContext : DbContext
+    {
+        public PizzaContext(DbContextOptions<PizzaContext> data) : base(data)
+        {
+
+        }
+
+        public DbSet<Pizza> Pizza { get; set; }
+
+    }
+}
---
+++ BlazingPizza2020/Models/Pizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza2020.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public int IdSize { get; set; }
+        public int IdOrder { get; set; }
+    }
+}
