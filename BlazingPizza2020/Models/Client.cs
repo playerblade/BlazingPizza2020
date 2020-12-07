5bd6727c56cce210a2c9c77d0abf788b888092cb
danielDelgado123 <delgadodaniel413@gmail.com>
2020-12-07 16:51:42 -0400

    Agregando nuevos modelos de las tablas

---
+++ BlazingPizza2020/Models/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza2020.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Ci { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+        public int Phone { get; set; }
+
+    }
+}
---
+++ BlazingPizza2020/Models/Size.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza2020.Models
+{
+    public class Size
+    {
+        public int Id { get; set; }
+        public string Size { get; set; }
+    }
+}
